118d1d4b5132a3910600a1e90917203f0a47ff07
songjooyoung <rtef23@nhn.com>
2018-04-19 14:01:14 +0900

    1. gulp eslint 기능 추가 테스트 2. gulp 컴파일 테스트

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 const gulp = require("gulp");
 const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
 
 gulp.task("default", function(){
-  gulp.src("public/**/*.js")
+  /*
+  * eslint를 사용하여 javascript를 분석하게 하는 코드
+  * */
+  // gulp
+  //   .src(["public/src/**/*.js"])
+  //   .pipe(eslint())
+  //   .pipe(eslint.format());
+
+  /*
+  * public/src 경로에 있는 javascript 파일들을
+  * es5문법으로 변경해서 public/dist에 저장하는 코드
+  * */
+  gulp
+    .src("public/src/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
---
+++ public/src/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function saySomething() {
+  let hiMessage = "hi es6";
+  let testSymbol = Symbol("this is test symbol");
+
+  console.log(`greetting : ${hiMessage}`);
+}
+
+saySomething();
